a3b8c8bfc8c285b14a3a0c327690a8baf9f33523
rsburke4 <37226458+rsburke4@users.noreply.github.com>
2020-07-14 17:23:18 -0400

    squached history

---
+++ Source/TestFace/TestFace.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "TestFace.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, TestFace, "TestFace" );
---
+++ Source/TestFace/TestFaceGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "TestFaceGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class TESTFACE_API ATestFaceGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
